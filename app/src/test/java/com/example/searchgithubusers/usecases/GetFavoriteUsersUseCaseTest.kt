0d354756976cea6abab50c04d6e7fcf407b04bcb
pawnjester <ebukeokonji@gmail.com>
2021-04-12 00:31:00 +0100

    - write tests for check get favorite use case

---
+++ app/src/test/java/com/example/searchgithubusers/usecases/GetFavoriteUsersUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.example.searchgithubusers.usecases
+
+import com.example.domain.repositories.GithubUsersRepository
+import com.example.domain.usecases.GetFavoriteUsersUseCase
+import com.example.searchgithubusers.fakes.DummyData
+import com.example.searchgithubusers.fakes.TestPostExecutionThread
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class GetFavoriteUsersUseCaseTest {
+
+    private lateinit var sut: GetFavoriteUsersUseCase
+
+    @Mock
+    lateinit var repository: GithubUsersRepository
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        sut = GetFavoriteUsersUseCase(repository, TestPostExecutionThread())
+    }
+
+
+    @Test
+    fun `get favorite users`() = runBlockingTest {
+        val user = DummyData.makeGithubUser()
+        val list = DummyData.makeGithubUserList(1)
+        repository.favoriteUser(user)
+        whenever(
+            repository.getFavoriteUsers()
+        ).thenReturn(
+            flow {
+                emit(
+                    list
+                )
+            }
+        )
+        sut.execute()
+        verify(repository).getFavoriteUsers()
+
+    }
+
+
+}
